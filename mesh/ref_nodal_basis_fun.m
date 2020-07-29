39371c5ee527c4021fbd0fc0806540db0eb4adfd
Ariel <ariels.boiardi@gmail.com>
2020-07-29 19:03:42 +0200

    Update ref_nodal_basis_fun.m

--- mesh/ref_nodal_basis_fun.m
+++ mesh/ref_nodal_basis_fun.m
@@ -2,6 +2,11 @@ function N = ref_nodal_basis_fun(idx, x1, x2)
 % ref_nodal_basis_fun computes the nodal basis functions on the reference
 % element in (x1,x2).
 
+if x1+x2 > 1 || x1 < 0 || x2<0
+    N=0;
+    return
+end
+
 switch idx
     case 1
         N = 1 - x1 - x2;
