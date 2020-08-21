42583079e8fdc40cf487dd16356fb9db2969b5bd
Ariel <ariels.boiardi@gmail.com>
2020-08-21 15:59:30 +0200

    Update test_global.m

--- test_global.m
+++ test_global.m
@@ -16,7 +16,7 @@ msh.show;
 msh.show_labels;
 
 %% Subdivision of triangles
-for idx=1:2
+for idx=1:4
 msh=msh.midsplitref;
 end
     
@@ -25,7 +25,7 @@ figure;
 problem.geometry.show('LineWidth',1.5,'EdgeColor','r')
 hold on; axis image off;
 msh.show
-% msh.show_labels
+msh.show_labels
 
 %% Assemble mass matrix
 
@@ -55,16 +55,17 @@ M=rmRows(M,DIR);
 % Discretization parameters
 Nt=100;
 Dt=(problem.time(2)-problem.time(1))/Nt;
-theta=0;
+theta=1;
 
 % Matrices for theta-method
-L = -M + theta*Dt*A;
+L = M/Dt + theta*A;
 LIND = rmCols(L,DIR);
-R = M + (1-theta)*Dt*A;
+PIND=pinv(LIND);
+R = -M/Dt + (1-theta)*A;
 
 %% Compute initial datum and try to plot it
 w0=INITeval(problem.initV, msh);
-w0(DIR(DIRid==1))=0; %%%%%% CHECK!!! Maybe linking initial condition and BC helps
+w0(DIR(DIRid==1))=0; 
 
 %% Test INTERPlinear
 % [X,Y]=meshgrid(sort(msh.nodes(:,1)),sort(msh.nodes(:,2)));
@@ -82,26 +83,30 @@ figure
 TO=triangulation(msh.elems, msh.nodes(:,1), msh.nodes(:,2), w0);
 trisurf(TO, 'FaceColor', 'w')
 axis equal
-title("Initial condition")
+title("Final condition")
 
 %% Advance solution in time by theta-method
 warning("Figures will be closed!")
-pause
+cont_flag=input("Do you want to solve the problem? [Y/n] \n",'s');
+if cont_flag=='n'
+    return
+end
+
 close all; clc
 
 for idx=1:Nt
-    w1DIR = DIReval(problem.bdcond, DIR, DIRid, msh, Dt*idx);
+    w1DIR = DIReval(problem.bdcond, DIR, DIRid, msh, Dt*(idx));
     F = -R*w0;
     B = -L*w1DIR;
-    w1IND = LIND \ (F+B);
+%     w1IND = LIND\(F+B);
+    w1IND = PIND * (F+B);
     w1 = mergeDIRINDvals(w1DIR, w1IND, IND);
     w0=w1;
     
     TO=triangulation(msh.elems, msh.nodes(:,1), msh.nodes(:,2), w0);
     
     trisurf(TO, 'EdgeAlpha',0.3)
-    axis equal
-    title(strcat("Solution (doubt it!) at time ", string(Dt*idx)))
+%     axis equal
+    title(strcat("Solution at time ", string(Dt*(idx))))
     pause(0.1)
 end
-
